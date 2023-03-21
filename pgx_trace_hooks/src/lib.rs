0e7a98d0cc19087d0e32c896f559e0b766933a16
Alex Chi <iskyzh@gmail.com>
2023-03-20 23:23:15 -0400

    feat: check if pgx_trace_hooks is loaded
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- pgx_trace_hooks/src/lib.rs
+++ pgx_trace_hooks/src/lib.rs
@@ -19,7 +19,13 @@ extern "C" fn executor_start_hook(query_desc: *mut pg_sys::QueryDesc, eflags: i3
 #[pg_guard]
 pub extern "C" fn _PG_init() {
   unsafe {
+    if !pg_sys::process_shared_preload_libraries_in_progress {
+      error!("pgx_trace_hooks is not in shared_preload_libraries");
+    }
+
     PREV_EXECUTOR_START_HOOK = pg_sys::ExecutorStart_hook;
     pg_sys::ExecutorStart_hook = Some(executor_start_hook);
   }
 }
+
+extension_sql!("LOAD 'pgx_trace_hooks';", name = "load_pgx_trace_hooks", requires = []);
