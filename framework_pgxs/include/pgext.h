a86ffce52a6f211aa2ecea417a87882055fd6385
Alex Chi <iskyzh@gmail.com>
2023-04-06 22:55:40 -0400

    only allow some undefined symbols
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- framework_pgxs/include/pgext.h
+++ framework_pgxs/include/pgext.h
@@ -1,8 +1,9 @@
 void _PG_init__pgext(void); // The original PG_init entry point
 
 // begin pgext APIs
-extern PGDLLIMPORT void __pgext_before_init(const char *name);
-extern PGDLLIMPORT void __pgext_after_init();
+extern PGDLLIMPORT void __attribute__((weak))
+__pgext_before_init(const char *name);
+extern PGDLLIMPORT void __attribute__((weak)) __pgext_after_init();
 // end pgext APIs
 
 void _PG_init() {
