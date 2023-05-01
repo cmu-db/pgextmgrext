28b7478b47f8f18aa1fe70cca03eac3be94b2503
Alex Chi <iskyzh@gmail.com>
2023-04-30 20:00:25 -0400

    refactor: use macros to dedup code (#38)
    * refactor: use macros to dedup code
    
    Signed-off-by: Alex Chi <iskyzh@gmail.com>
    
    * clippy
    
    Signed-off-by: Alex Chi <iskyzh@gmail.com>
    
    ---------
    
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- pgextmgr/src/hook_pregen.rs
+++ pgextmgr/src/hook_pregen.rs
@@ -4,125 +4,85 @@
 
 use std::ffi::c_int;
 
+use pgext_hook_macros::*;
 use pgrx::pg_sys::*;
 use pgrx::prelude::*;
 
-macro_rules! generate_planner_hook {
-  ( $($id:tt),* ) => {
-    $(
-      paste::paste! {
-        #[pg_guard]
-        unsafe extern "C" fn [< __pgext_planner_hook_ $id >](
-          parse: *mut Query,
-          query_string: *const ::std::os::raw::c_char,
-          cursor_options: ::std::os::raw::c_int,
-          bound_params: ParamListInfo,
-        ) -> *mut PlannedStmt {
-          crate::hook_ext::pgext_planner_hook_cb($id, parse, query_string, cursor_options, bound_params)
-        }
-      }
-    )*
+use crate::hook_ext::*;
 
-    pub static PREGENERATED_PLANNER_HOOKS: &[planner_hook_type] = &[
-      $(
-        paste::paste! { Some([< __pgext_planner_hook_ $id >]) }
-      ),*
-    ];
+macro_rules! build_hook_function {
+  ([ $prefix:ident, $id:tt, $cb:ident, ($ret:ty) ] { $( $param:ident : $t:ty ,)* }) => {
+    paste::paste! {
+      #[pg_guard]
+      unsafe extern "C" fn [< $prefix _ $id >](
+        $( $param : $t ),*
+      ) -> $ret {
+        $cb($id, $( $param ),*)
+      }
+    }
   };
 }
 
-macro_rules! generate_executor_start_hook {
-  ( $($id:tt),* ) => {
+macro_rules! generate_hooks {
+  ( [ $prefix:ident, $cb:ident, $global:ident, $hook_type:ty, $params:ident, $retty:tt ] $($id:tt),* ) => {
     $(
-      paste::paste! {
-        #[pg_guard]
-        unsafe extern "C" fn [< __pgext_executor_start_hook_ $id >](
-          query_desc: *mut QueryDesc, eflags: c_int,
-        ) {
-          crate::hook_ext::pgext_executor_start_hook_cb($id, query_desc, eflags)
-        }
-      }
+      $params! { [ $prefix, $id, $cb, $retty ] build_hook_function }
     )*
-
-    pub static PREGENERATED_EXECUTOR_START_HOOKS: &[ExecutorStart_hook_type] = &[
+    pub static $global: &[$hook_type] = &[
       $(
-        paste::paste! { Some([< __pgext_executor_start_hook_ $id >]) }
-      ),*
-    ];
-  };
-}
-
-macro_rules! generate_executor_run_hook {
-  ( $($id:tt),* ) => {
-    $(
-      paste::paste! {
-        #[pg_guard]
-        unsafe extern "C" fn [< __pgext_executor_run_hook_ $id >](
-          query_desc: *mut QueryDesc, direction: ScanDirection, count: uint64, execute_once: bool,
-        ) {
-          crate::hook_ext::pgext_executor_run_hook_cb($id, query_desc, direction, count, execute_once)
-        }
-      }
-    )*
-
-    pub static PREGENERATED_EXECUTOR_RUN_HOOKS: &[ExecutorRun_hook_type] = &[
-      $(
-        paste::paste! { Some([< __pgext_executor_run_hook_ $id >]) }
-      ),*
-    ];
-  };
-}
-
-macro_rules! generate_executor_finish_hook {
-  ( $($id:tt),* ) => {
-    $(
-      paste::paste! {
-        #[pg_guard]
-        unsafe extern "C" fn [< __pgext_executor_finish_hook_ $id >](
-          query_desc: *mut QueryDesc,
-        ) {
-          crate::hook_ext::pgext_executor_finish_hook_cb($id, query_desc)
-        }
-      }
-    )*
-
-    pub static PREGENERATED_EXECUTOR_FINISH_HOOKS: &[ExecutorFinish_hook_type] = &[
-      $(
-        paste::paste! { Some([< __pgext_executor_finish_hook_ $id >]) }
-      ),*
-    ];
-  };
-}
-
-macro_rules! generate_executor_end_hook {
-  ( $($id:tt),* ) => {
-    $(
-      paste::paste! {
-        #[pg_guard]
-        unsafe extern "C" fn [< __pgext_executor_end_hook_ $id >](
-          query_desc: *mut QueryDesc,
-        ) {
-          crate::hook_ext::pgext_executor_end_hook_cb($id, query_desc)
-        }
-      }
-    )*
-
-    pub static PREGENERATED_EXECUTOR_END_HOOKS: &[ExecutorEnd_hook_type] = &[
-      $(
-        paste::paste! { Some([< __pgext_executor_end_hook_ $id >]) }
+        paste::paste! { Some([< $prefix _ $id >]) }
       ),*
     ];
   };
 }
 
 macro_rules! generate_copies {
-  ($t:tt) => {
-    $t! { 1, 2, 3, 4, 5 }
+  ([$($x:tt),*] $t:tt) => {
+    $t! { [$($x),*] 1, 2, 3, 4, 5 }
   };
 }
 
-generate_copies! { generate_planner_hook }
-generate_copies! { generate_executor_start_hook }
-generate_copies! { generate_executor_run_hook }
-generate_copies! { generate_executor_finish_hook }
-generate_copies! { generate_executor_end_hook }
+generate_copies! { [
+  __pgext_planner_hook,
+  pgext_planner_hook_cb,
+  PREGENERATED_PLANNER_HOOKS,
+  planner_hook_type,
+  planner_hook_params,
+  (*mut PlannedStmt)
+] generate_hooks }
+
+generate_copies! { [
+  __pgext_executor_start_hook,
+  pgext_executor_start_hook_cb,
+  PREGENERATED_EXECUTOR_START_HOOKS,
+  ExecutorStart_hook_type,
+  executor_start_hook_params,
+  (())
+] generate_hooks }
+
+generate_copies! { [
+  __pgext_executor_run_hook,
+  pgext_executor_run_hook_cb,
+  PREGENERATED_EXECUTOR_RUN_HOOKS,
+  ExecutorRun_hook_type,
+  executor_run_hook_params,
+  (())
+] generate_hooks }
+
+generate_copies! { [
+  __pgext_executor_finish_hook,
+  pgext_executor_finish_hook_cb,
+  PREGENERATED_EXECUTOR_FINISH_HOOKS,
+  ExecutorFinish_hook_type,
+  executor_finish_hook_params,
+  (())
+] generate_hooks }
+
+generate_copies! { [
+  __pgext_executor_end_hook,
+  pgext_executor_end_hook_cb,
+  PREGENERATED_EXECUTOR_END_HOOKS,
+  ExecutorEnd_hook_type,
+  executor_end_hook_params,
+  (())
+] generate_hooks }
